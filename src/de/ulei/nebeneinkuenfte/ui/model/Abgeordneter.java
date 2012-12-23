dc90d7491a28a41163d8c052f609f861470533fe
mrkhfloppy <mrkhfloppy@googlemail.com>
2012-12-23 14:33:02 +0100

    added faction resource to RDF model

--- src/de/ulei/nebeneinkuenfte/ui/model/Abgeordneter.java
+++ src/de/ulei/nebeneinkuenfte/ui/model/Abgeordneter.java
@@ -6,16 +6,17 @@ import java.util.List;
 
 import org.apache.commons.lang3.StringEscapeUtils;
 
+import de.ulei.nebeneinkuenfte.util.IConstants;
+
 public class Abgeordneter implements Serializable {
 
 	@Override
 	public String toString() {
-		return "Abgeordneter [uri=" + uri + ", forename=" + forename
-				+ ", lastname=" + lastname + "]";
+		return "Abgeordneter [uri=" + uri + ", forename=" + forename + ", lastname=" + lastname + "]";
 	}
 
 	private static final long serialVersionUID = 1447162980745311414L;
-	
+
 	private String uri;
 	private String label;
 	private String forename;
@@ -24,6 +25,7 @@ public class Abgeordneter implements Serializable {
 	private String email;
 	private String wahlkreisUri;
 	private String wahlkreisName;
+	private String fraktionUri;
 	private String fraktion;
 	private int anzahlNebeneinkuenfte = 0;
 	private int minZusatzeinkommen = 0;
@@ -33,14 +35,14 @@ public class Abgeordneter implements Serializable {
 	public Abgeordneter() {
 		nebentaetigkeiten = new ArrayList<Nebentaetigkeit>();
 	}
-	
+
 	public void setAttributesFromLinkText(String linkText) {
 		String[] values = linkText.split(",");
-		if(values.length == 3) {
-			this.setLastname(StringEscapeUtils.unescapeHtml4(values[0]));
-			this.forename = StringEscapeUtils.unescapeHtml4(values[1]).trim();
-			this.fraktion = StringEscapeUtils.unescapeHtml4(values[2]).trim();
-			this.label = this.forename + "+" + this.lastname + "+" + this.fraktion.replaceAll(" ", "+");
+		if (values.length == 3) {
+			setLastname(StringEscapeUtils.unescapeHtml4(values[0]));
+			setForename(StringEscapeUtils.unescapeHtml4(values[1]).trim());
+			setFraktion(StringEscapeUtils.unescapeHtml4(values[2]).trim());
+			setLabel(this.forename + "+" + this.lastname + "+" + this.fraktion.replaceAll(" ", "+"));
 		} else {
 			System.out.println("Problem setting attributes from: " + linkText);
 		}
@@ -75,7 +77,7 @@ public class Abgeordneter implements Serializable {
 	}
 
 	public void setLastname(String lastname) {
-		if(lastname.contains("(")) {
+		if (lastname.contains("(")) {
 			lastname = lastname.substring(0, lastname.indexOf("(")).trim();
 		}
 		this.lastname = lastname;
@@ -104,7 +106,7 @@ public class Abgeordneter implements Serializable {
 	public void setWahlkreisUri(String wahlkreisUri) {
 		this.wahlkreisUri = wahlkreisUri;
 	}
-	
+
 	public String getWahlkreisName() {
 		return wahlkreisName;
 	}
@@ -112,12 +114,37 @@ public class Abgeordneter implements Serializable {
 	public void setWahlkreisName(String wahlkreisName) {
 		this.wahlkreisName = wahlkreisName;
 	}
-	
+
+	public String getFraktionUri() {
+		return fraktionUri;
+	}
+
+	public void setFraktionUri(String fraktionUri) {
+		this.fraktionUri = fraktionUri;
+	}
+
 	public String getFraktion() {
 		return fraktion;
 	}
 
 	public void setFraktion(String fraktion) {
+
+		if (fraktion == null) {
+			this.fraktion = "unbekannt";
+			return;
+		}
+
+		if (fraktion.equals(IConstants.SPD_LABEL))
+			setFraktionUri(IConstants.SPD_FRAKTION);
+		else if (fraktion.equals(IConstants.CDU_CSU_LABEL))
+			setFraktionUri(IConstants.CDU_CSU_FRAKTION);
+		else if (fraktion.equals(IConstants.DIE_LINKE_LABEL))
+			setFraktionUri(IConstants.DIE_LINKE_FRAKTION);
+		else if (fraktion.equals(IConstants.FDP_LABEL))
+			setFraktionUri(IConstants.FDP_FRAKTION);
+		else if (fraktion.equals(IConstants.GRUENE_LABEL))
+			setFraktionUri(IConstants.GRUENE_FRAKTION);
+
 		this.fraktion = fraktion;
 	}
 
@@ -152,28 +179,26 @@ public class Abgeordneter implements Serializable {
 	public void setNebentaetigkeiten(List<Nebentaetigkeit> nebentaetigkeiten) {
 		this.nebentaetigkeiten = nebentaetigkeiten;
 		boolean canMax = true;
-		for(Nebentaetigkeit nt : nebentaetigkeiten) {
-			if(nt.getStufe()!=null) {
-				if(nt.getStufe().contains("1")) {
-					this.minZusatzeinkommen+=1000;
-					this.maxZusatzeinkommen+=3499;
-				} else if(nt.getStufe().contains("2")) {
-					this.minZusatzeinkommen+=3500;
-					this.maxZusatzeinkommen+=6999;
-				} else if(nt.getStufe().contains("3")) {
-					this.minZusatzeinkommen+=7000;
-					canMax=false;
+		for (Nebentaetigkeit nt : nebentaetigkeiten) {
+			if (nt.getStufe() != null) {
+				if (nt.getStufe().contains("1")) {
+					this.minZusatzeinkommen += 1000;
+					this.maxZusatzeinkommen += 3499;
+				} else if (nt.getStufe().contains("2")) {
+					this.minZusatzeinkommen += 3500;
+					this.maxZusatzeinkommen += 6999;
+				} else if (nt.getStufe().contains("3")) {
+					this.minZusatzeinkommen += 7000;
+					canMax = false;
 				}
 			} else {
-				this.maxZusatzeinkommen+=1000;
+				this.maxZusatzeinkommen += 1000;
 			}
 		}
-		if(!canMax) {
-			this.maxZusatzeinkommen=1000000;
+		if (!canMax) {
+			this.maxZusatzeinkommen = 1000000;
 		}
 		this.setAnzahlNebeneinkuenfte(nebentaetigkeiten.size());
 	}
-	
-	
-	
+
 }
--- src/de/ulei/nebeneinkuenfte/util/IConstants.java
+++ src/de/ulei/nebeneinkuenfte/util/IConstants.java
@@ -1,40 +1,67 @@
 package de.ulei.nebeneinkuenfte.util;
 
 public interface IConstants {
+	
+	/*
+	 * IDs for different views
+	 */
 
 	public static int PERSON_BASIC_VIEW = 0;
 	public static int PERSON_PERSON_VIEW = 1;
 	public static int PERSON_PARTY_VIEW = 2;
 	public static int PERSON_ORIGIN_VIEW = 3;
 
+	/*
+	 * common button length
+	 */
+	
 	public static int BUTTON_WIDTH = 150;
 
-	public static String[] ABGEORDNETER_VISIBLE_COLUMNS = new String[] {
-			"forename", "lastname", "homepage", "email", "fraktion",
-			"wahlkreisUri", "minZusatzeinkommen", "maxZusatzeinkommen" };
+	
+	/*
+	 * label and URI for political partys
+	 */
+	
+	public final String SPD_FRAKTION = "http://www.spdfraktion.de/";
+	public final String SPD_LABEL = "SPD";
 
-	public static String[] ABGEORDNETER_COLUMN_HEADER = new String[] {
-			"Vorname", "Nachname", "Homepage", "Email", "Partei", "Wahlkreis",
-			"Zusatzeinkommen (min)", "Zusatzeinkommen (max)" };
+	public final String CDU_CSU_FRAKTION = "http://www.cducsu.de/";
+	public final String CDU_CSU_LABEL = "CDU/CSU";
 
-	public static String[] NEBENTAETIGKEIT_VISIBLE_COLUMNS = new String[] {
-			"auftraggeber", "type", "place", "year", "stufe" };
+	public final String DIE_LINKE_FRAKTION = "http://www.linksfraktion.de/";
+	public final String DIE_LINKE_LABEL = "Die Linke";
 
-	public static String[] NEBENTAETIGKEIT_COLUMN_HEADER = new String[] {
-			"Auftraggeber", "Art", "Ort", "Jahr", "Stufe" };
+	public final String FDP_FRAKTION = "http://www.fdp-fraktion.de/";
+	public final String FDP_LABEL = "FDP";
 
-	public static String[] FRAKTION_VISIBLE_COLUMNS = new String[] {
-			"forename", "lastname", "auftraggeber", "type",
+	public final String GRUENE_FRAKTION = "http://www.gruene-bundestag.de/";
+	public final String GRUENE_LABEL = "Bündnis 90/Die Grünen";
+	
+	/*
+	 * column order and headers for tables in according views
+	 */
+
+	public static String[] ABGEORDNETER_VISIBLE_COLUMNS = new String[] { "forename", "lastname", "homepage", "email",
+			"fraktion", "wahlkreisUri", "minZusatzeinkommen", "maxZusatzeinkommen" };
+
+	public static String[] ABGEORDNETER_COLUMN_HEADER = new String[] { "Vorname", "Nachname", "Homepage", "Email",
+			"Fraktion", "Wahlkreis", "Zusatzeinkommen (min)", "Zusatzeinkommen (max)" };
+
+	public static String[] NEBENTAETIGKEIT_VISIBLE_COLUMNS = new String[] { "auftraggeber", "type", "place", "year",
+			"stufe" };
+
+	public static String[] NEBENTAETIGKEIT_COLUMN_HEADER = new String[] { "Auftraggeber", "Art", "Ort", "Jahr", "Stufe" };
+
+	public static String[] FRAKTION_VISIBLE_COLUMNS = new String[] { "forename", "lastname", "auftraggeber", "type",
 			"place", "year", "stufe" };
 
-	public static String[] FRAKTION_COLUMN_HEADER = new String[] { "Vorname",
-			"Nachname", "Auftraggeber", "Art", "Ort", "Jahr", "Stufe" };
+	public static String[] FRAKTION_COLUMN_HEADER = new String[] { "Vorname", "Nachname", "Auftraggeber", "Art", "Ort",
+			"Jahr", "Stufe" };
 
-	public static String[] AUFTRAGGEBER_VISIBLE_COLUMNS = new String[] {
-			"forename", "lastname", "fraktion", "type",
+	public static String[] AUFTRAGGEBER_VISIBLE_COLUMNS = new String[] { "forename", "lastname", "fraktion", "type",
 			"place", "year", "stufe" };
 
-	public static String[] AUFTRAGGEBER_COLUMN_HEADER = new String[] { "Vorname",
-			"Nachname", "Partei", "Art", "Ort", "Jahr", "Stufe" };
+	public static String[] AUFTRAGGEBER_COLUMN_HEADER = new String[] { "Vorname", "Nachname", "Fraktion", "Art", "Ort",
+			"Jahr", "Stufe" };
 
 }
