dd1b8bae184a8d580f179fb5c7ebad176f829c82
mrkhfloppy <mrkhfloppy@googlemail.com>
2013-01-20 20:58:55 +0100

    add missing files

---
+++ src/de/ulei/nebeneinkuenfte/ui/TabListener.java
@@ -0,0 +1,45 @@
+package de.ulei.nebeneinkuenfte.ui;
+
+import com.vaadin.ui.TabSheet.SelectedTabChangeEvent;
+import com.vaadin.ui.TabSheet.SelectedTabChangeListener;
+
+import de.ulei.nebeneinkuenfte.util.IConstants;
+
+public class TabListener implements SelectedTabChangeListener {
+
+	private static final long serialVersionUID = 4906571421432304590L;
+
+	@Override
+	public void selectedTabChange(SelectedTabChangeEvent event) {
+
+		String tabCaption = event.getTabSheet().getSelectedTab().getCaption();
+
+		if (tabCaption != null) {
+			int tabIndex = Integer.valueOf(tabCaption);
+			switch (tabIndex) {
+			case IConstants.PERSON_BASIC_VIEW:
+				NebeneinkuenfteApplication.getInstance().setURIFragment(IConstants.PERSON_BASIC_VIEW_FRAG);
+				break;
+			case IConstants.PERSON_PERSON_VIEW:
+				NebeneinkuenfteApplication.getInstance().setURIFragment(IConstants.PERSON_PERSON_VIEW_FRAG);
+				break;
+			case IConstants.PERSON_PARTY_VIEW:
+				NebeneinkuenfteApplication.getInstance().setURIFragment(IConstants.PERSON_PARTY_VIEW_FRAG);
+				break;
+			case IConstants.PERSON_ORIGIN_VIEW:
+				NebeneinkuenfteApplication.getInstance().setURIFragment(IConstants.PERSON_ORIGIN_VIEW_FRAG);
+				break;
+			case IConstants.IMPRESSUM_VIEW:
+				NebeneinkuenfteApplication.getInstance().setURIFragment(IConstants.IMPRESSUM_VIEW_FRAG);
+				break;
+			case IConstants.ABOUT_PROJECT_VIEW:
+				NebeneinkuenfteApplication.getInstance().setURIFragment(IConstants.ABOUT_PROJECT_VIEW_FRAG);
+				break;
+
+			default:
+				break;
+			}
+		}
+
+	}
+}
