13d854740dfe3d50791a530ce7ae71c2a5250740
mrkhfloppy <mrkhfloppy@localhost>
2013-05-08 09:07:56 +0200

    introduce abstract superClass for text-only-views like analysis or impressum

--- src/de/ulei/nebeneinkuenfte/ui/view/AboutProjectView.java
+++ src/de/ulei/nebeneinkuenfte/ui/view/AboutProjectView.java
@@ -2,19 +2,16 @@ package de.ulei.nebeneinkuenfte.ui.view;
 
 import java.io.Serializable;
 
-import com.vaadin.ui.GridLayout;
-import com.vaadin.ui.Label;
-
 import de.ulei.nebeneinkuenfte.util.IConstants;
 
-public class AboutProjectView extends GridLayout implements Serializable {
+public class AboutProjectView extends AbstractTextView implements Serializable {
 
 	private static final long serialVersionUID = -6253578581349325665L;
 
 	public AboutProjectView() {
 
-		setCaption(String.valueOf(IConstants.ABOUT_PROJECT_VIEW));
-		addComponent(new Label("In Arbeit"));
+		super(IConstants.ABOUT_PROJECT_VIEW);
+		addHeadline("In Arbeit", 1);
 
 	}
 
--- src/de/ulei/nebeneinkuenfte/ui/view/ImpressumView.java
+++ src/de/ulei/nebeneinkuenfte/ui/view/ImpressumView.java
@@ -2,19 +2,16 @@ package de.ulei.nebeneinkuenfte.ui.view;
 
 import java.io.Serializable;
 
-import com.vaadin.ui.GridLayout;
-import com.vaadin.ui.Label;
-
 import de.ulei.nebeneinkuenfte.util.IConstants;
 
-public class ImpressumView extends GridLayout implements Serializable {
+public class ImpressumView extends AbstractTextView implements Serializable {
 
 	private static final long serialVersionUID = -3207647187091883100L;
 
 	public ImpressumView() {
 
-		setCaption(String.valueOf(IConstants.IMPRESSUM_VIEW));
-		addComponent(new Label("In Arbeit"));
+		super(IConstants.IMPRESSUM_VIEW);
+		addHeadline("In Arbeit", 1);
 
 	}
 
